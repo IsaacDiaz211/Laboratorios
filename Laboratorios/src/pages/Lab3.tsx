3bb31c08de3ad93e12f6c9d9563792087b68cc3b
Federico Pérez <fedepruniversidad@gmail.com>
2025-09-08 12:06:21 -0300

    LAB3

--- Laboratorios/src/pages/Lab3.tsx
+++ Laboratorios/src/pages/Lab3.tsx
@@ -1,9 +1,52 @@
-const Lab3 = () => {
-    return (
-        <div>
-            <h2>Laboratorio 1</h2>
-        </div>
-    )
-};
+import { Tabs } from 'antd';
+//import Ex1 from '../exercises/lab3/Ex1';
+//import Ex2 from '../exercises/lab3/Ex2';
+//import Ex3 from '../exercises/lab3/Ex3';
+
+function Lab3() {
+  return (
+    <Tabs
+      tabPosition="left"
+      items={[
+        {
+          label: "Ejercicio 1",
+          key: "1",
+          children: 
+            <div style={{
+              display: "flex",
+              justifyContent: "center",
+              paddingTop: "2rem",
+            }}>
+              
+            </div>,
+        },
+        {
+          label: "Ejercicio 2",
+          key: "2",
+          children:
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                paddingTop: "2rem",
+              }}>
+          
+            </div>,
+        },
+        {
+          label: "Ejercicio 3",
+          key: "3",
+          children:
+            <div style={{
+                display: "flex",
+                justifyContent: "center",
+                paddingTop: "2rem",
+              }}>
+
+            </div>,
+        },
+      ]}
+    />
+  );
+}
 
 export default Lab3;

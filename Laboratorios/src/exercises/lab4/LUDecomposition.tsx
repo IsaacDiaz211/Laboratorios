da5ce046f40196602b5dcf4d37e04cf0935b7174
Isaac <isaac211.md@outlook.com>
2025-10-11 22:56:23 -0300

    decomposition lu

--- Laboratorios/src/exercises/lab4/LUDecomposition.tsx
+++ Laboratorios/src/exercises/lab4/LUDecomposition.tsx
@@ -10,25 +10,75 @@ function LUDecomposition() {
 
   return (
     <div>
-      <SubTab graph={true} onChange={setTab} value={tab} />
+      <SubTab graph={false} onChange={setTab} value={tab} />
       {tab === "interfaz" && <LUDecompositionUI />}
       {tab === "seudo" && (
         <Seudo code={``} />
       )}      
       {tab === "codigo" && (
-        <CodeBlock code={``} />
+        <CodeBlock code={`function LU_decomposition(matrix: number[][], b: number[]): number[] | null {
+    const n = matrix.length;
+
+    //Verificaciones
+    if(n !== b.length) {
+        return null;
+    }
+    if (n === 0) return [];
+    if (!matrix.every((row) => row.length === n)) {
+        throw new Error("La matriz A debe ser cuadrada n x n");
+    }
+    if(matrix[0][0] === 0) {
+        return null;
+    }
+
+    // Obtener la matriz U y L
+    let U = matrix.map(r => r.slice());
+    let L: number[][] = new Array(n).fill(0).map(() => new Array(n).fill(0));
+    for (let k = 0; k < n; k++) {
+        L[k][k] = 1;
+    }
+    for (let j = 0; j < n; j++){
+        for (let i = j + 1; i < n; i++) {
+            if(U[j][j] === 0) {
+                return null;
+            }
+            let factor: number = U[i][j] / U[j][j];
+            let vector: number[] = U[j].map(val => factor * val);
+            U[i] = U[i].map((val, k) => val - vector[k]);
+            L[i][j] = factor;
+            if(U[i][j] !== 0) {     
+                return null;
+            }
+            i++;
+        }
+        j++;
+    }
+
+    //Obtenemos y según L.y = b
+    let y: number[] = new Array(n).fill(0);
+    y[0] = b[0];
+    for(let k = 1; k < n; k++){
+        let sum: number = 0;
+        for (let l = 0; l < k; l++) {
+            sum += L[k][l] * y[l];
+        }
+        y[k] = (b[k] - sum);
+    }
+
+    //Obtenemos x según U.x = y
+    let x: number[] = new Array(n).fill(0);
+    for(let k = n-1; k >= 0; k--){
+        let sum: number = 0;
+        for (let l = k + 1; l <n; l++) {
+            sum += U[k][l] * x[l];
+        }
+        x[k] = (y[k] - sum) / U[k][k];
+    }
+
+    return x;
+}`} />
       )}
-      {tab === "grafico" && (
-        
-        <CartesianGraph
-          xRange={[-5, 10]}
-          yRange={[-30, 30]}
-          functionString="Math.PI*(x**2)*((9-x)/3) - 30"
-          expresion="\pi * h^2 (\frac{3R - h}{3})"
-          step={0.1}
-        />
       
-      )}
     </div>
   );
 };
--- Laboratorios/src/exercises/lab4/algos_SystemOfLinearEquations.ts
+++ Laboratorios/src/exercises/lab4/algos_SystemOfLinearEquations.ts
@@ -23,8 +23,8 @@ function algo_gauss_elimination(matrix: number[][], b: number[]): number[] | nul
     
     // Eliminación hacia adelante
     const A = matrix.map((row, i) => [...row, b[i]]); // Matriz ampliada
-    while ( j < n){
-        while (i < n) {
+    for (let j = 0; j < n; j++){
+        for (let i = j + 1; i < n; i++) {
             if(A[j][j] === 0) {
                 return null;
             }
@@ -37,7 +37,6 @@ function algo_gauss_elimination(matrix: number[][], b: number[]): number[] | nul
             i++;
         }
         j++;
-        i = j + 1;
     }
 
     // Sustitución hacia atrás
@@ -54,3 +53,67 @@ function algo_gauss_elimination(matrix: number[][], b: number[]): number[] | nul
 }
 
 export { algo_gauss_elimination };
+
+function algo_LU_decomposition(matrix: number[][], b: number[]): number[] | null {
+    const n = matrix.length;
+
+    //Verificaciones
+    if(n !== b.length) {
+        return null;
+    }
+    if (n === 0) return [];
+    if (!matrix.every((row) => row.length === n)) {
+        throw new Error("La matriz A debe ser cuadrada n x n");
+    }
+    if(matrix[0][0] === 0) {
+        return null;
+    }
+
+    // Obtener la matriz U y L
+    let U = matrix.map(r => r.slice());
+    let L: number[][] = new Array(n).fill(0).map(() => new Array(n).fill(0));
+    for (let k = 0; k < n; k++) {
+        L[k][k] = 1;
+    }
+    for (let j = 0; j < n; j++){
+        for (let i = j + 1; i < n; i++) {
+            if(U[j][j] === 0) {
+                return null;
+            }
+            let factor: number = U[i][j] / U[j][j];
+            let vector: number[] = U[j].map(val => factor * val);
+            U[i] = U[i].map((val, k) => val - vector[k]);
+            L[i][j] = factor;
+            if(U[i][j] !== 0) {     
+                return null;
+            }
+            i++;
+        }
+        j++;
+    }
+
+    //Obtenemos y según L.y = b
+    let y: number[] = new Array(n).fill(0);
+    y[0] = b[0];
+    for(let k = 1; k < n; k++){
+        let sum: number = 0;
+        for (let l = 0; l < k; l++) {
+            sum += L[k][l] * y[l];
+        }
+        y[k] = (b[k] - sum);
+    }
+
+    //Obtenemos x según U.x = y
+    let x: number[] = new Array(n).fill(0);
+    for(let k = n-1; k >= 0; k--){
+        let sum: number = 0;
+        for (let l = k + 1; l <n; l++) {
+            sum += U[k][l] * x[l];
+        }
+        x[k] = (y[k] - sum) / U[k][k];
+    }
+
+    return x;
+}
+
+export {algo_LU_decomposition}
